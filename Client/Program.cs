d38e933da422ecfdbafc09579ebf8c523e02bc9c
Екатерина Голумбиевская <golumbiyevskaya90@bk.ru>
2026-06-11 02:42:22 +0800

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.Sockets;
+using System.Net;
+
+namespace Client
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.Title = "Client";
+            Console.WriteLine("CLIENT");
+			try
+			{
+				//1) определяем параметры подключения:
+				IPAddress target_address;
+				IPAddress.TryParse("127.0.0.1", out target_address);
+				IPEndPoint target = new IPEndPoint(target_address, 27015);
+				//2)Создаем сокет для подключения к Серверу$
+				Socket connect_socket = new Socket(target.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+				//3) Подключаемся к серверу:
+				connect_socket.Connect(target);
+				//4)Производим обмен данными:
+				//do 
+				//{
+				//4.1) Отправка:
+				string message = "Hello Server!";
+				byte[] bytes = Encoding.UTF8.GetBytes(message);
+				connect_socket.Send(bytes);
+				Console.WriteLine($"{bytes.Length} Bytes sent");
+				//4.2) Ожидаем подверждение от Сервера:
+				byte[] buffer = new byte[1024];
+				int received = connect_socket.Receive(buffer);
+				string response = Encoding.UTF8.GetString(buffer);
+				Console.WriteLine($"$Recived {received} Bytes from Server. Message: {response}");
+				//if(response =="<ACK>") Console.WriteLine("Server acknowledge message");
+				//}while(true);
+				//5) Разрываем TCP соединение:
+				connect_socket.Shutdown(SocketShutdown.Both);
+				//6) Закрываем сокет:
+				connect_socket.Close();
+			}
+			catch (Exception ex)
+			{
+                Console.WriteLine(ex.Message);
+			}
+			Console.ReadLine();
+		}
+	}
+}
